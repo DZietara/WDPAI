a35fc62fb8155ca2d28f843517b80a61797835f6
Damian Zietara <zietara.damian@interia.pl>
2022-05-20 16:38:51 +0200

    Added register layout for desktop

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -9,6 +9,16 @@ class DashboardController extends AppController {
         $this->render("login");
     }
 
+    public function login()
+    {
+        $this->render("login");
+    }
+
+    public function register()
+    {
+        $this->render("register");
+    }
+
     public function dashboard() {
         // TODO return and render display.html
         $hello = 'Welcome on Dahboard page!';
--- index.php
+++ index.php
@@ -7,5 +7,7 @@ $path = trim($_SERVER['REQUEST_URI'], '/');
 Router::get('', 'DashboardController');
 Router::get('dashboard', 'DashboardController');
 Router::get('projects', 'ProjectsController');
+Router::get('login', 'DashboardController');
+Router::get('register', 'DashboardController');
 
 Router::run($path);
