989c16849251aa69912b2a4d14efe0b1a3b28d3e
Damian Zietara <zietara.damian@interia.pl>
2022-06-21 00:16:53 +0200

    Add validation on the frontend

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,28 @@
 const form = document.querySelector("form");
+const nameInput = form.querySelector('input[name="name"]')
+const surnameInput = form.querySelector('input[name="surname"]')
 const emailInput = form.querySelector('input[name="email"]');
+const passwordInput = form.querySelector('input[name="password"]');
 const confirmedPasswordInput = form.querySelector('input[name="confirmedPassword"]');
 
 function isEmail(email) {
     return /\S+@\S+\.\S+/.test(email);
 }
 
+function nameIsNotEmpty(name) {
+    return /[A-Za-z]+/.test(name);
+}
+
+function surnameIsNotEmpty(surname) {
+    return /[A-Za-z]+/.test(surname);
+}
+
 function arePasswordsSame(password, confirmedPassword) {
     return password === confirmedPassword;
+}
+
+function requiredLengthOfPassword(password) {
+    return password.length >= 6;
 }
 
 function markValidation(element, condition) {
@@ -22,7 +37,31 @@ function validateEmail() {
     );
 }
 
+function validateName() {
+    setTimeout(function () {
+            markValidation(nameInput, nameIsNotEmpty(nameInput.value));
+        },
+        1000
+    );
+}
+
+function validateSurname() {
+    setTimeout(function () {
+            markValidation(surnameInput, surnameIsNotEmpty(surnameInput.value));
+        },
+        1000
+    );
+}
+
 function validatePassword() {
+    setTimeout(function () {
+            markValidation(passwordInput, requiredLengthOfPassword(passwordInput.value))
+        },
+        1000
+    );
+}
+
+function validateConfirmedPassword() {
     setTimeout(function () {
             const condition = arePasswordsSame(
                 confirmedPasswordInput.previousElementSibling.value,
@@ -35,4 +74,7 @@ function validatePassword() {
 }
 
 emailInput.addEventListener('keyup', validateEmail);
-confirmedPasswordInput.addEventListener('keyup', validatePassword);
+nameInput.addEventListener('keyup', validateName);
+surnameInput.addEventListener('keyup', validateSurname);
+passwordInput.addEventListener('keyup', validatePassword);
+confirmedPasswordInput.addEventListener('keyup', validateConfirmedPassword);
