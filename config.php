dcefed0553eec55f9c6fa42e8bb4ccd9718c3d55
DZietara <39183654+DZietara@users.noreply.github.com>
2022-06-28 15:27:43 +0200

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-const USERNAME ='lqyfejeclxywfi';
-const PASSWORD ='cb8232e93cf5b7ac37b8f3be2feeab2c3288176c41c9246993c63b95cb667878';
-const HOST ='ec2-34-242-8-97.eu-west-1.compute.amazonaws.com';
-const DATABASE ='d4qt7218q7eog6';
